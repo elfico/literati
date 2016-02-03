f9e421289c393fa4b0f56df140f897187d046158
Asamu Adebayo <asamu.a.adebayo@gmail.com>
2016-02-03 12:38:11 +0100

    poetry added

---
+++ Literati/Areas/Poetry/PoetryAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Literati.Areas.Poetry
+{
+    public class PoetryAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Poetry";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Poetry_default",
+                "Poetry/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Literati/Controllers/PoetryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Literati.Controllers
+{
+    public class PoetryController : Controller
+    {
+        // GET: Poetry
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
